3bfbc2a329d1be354d31e5be31791b767683eb99
Richard Meyer <richie.p.meyer@gmail.com>
2023-02-02 09:58:05 -0600

    update

--- explore.py
+++ explore.py
@@ -125,8 +125,8 @@ def cluster_Xsets(train,val,test,cols):
     return train,val,test
 
 def add_exploration_columns(df):
-    df['acid_alc_sugar']=(df['fixed_acidity']+df['volatile_acidity']+df['citric_acid']/3)/(df['alcohol']+df['residual_sugar']/2)
-    df['acid_chlor'] = (df['fixed_acidity']+df['volatile_acidity']+df['citric_acid']/3)/df['chlorides']
+    df['acid_alc_sugar']=((df['fixed_acidity']+df['volatile_acidity']+df['citric_acid'])/3)/((df['alcohol']+df['residual_sugar'])/2)
+    df['acid_chlor'] = ((df['fixed_acidity']+df['volatile_acidity']+df['citric_acid'])/3)/df['chlorides']
     return df
    
     
